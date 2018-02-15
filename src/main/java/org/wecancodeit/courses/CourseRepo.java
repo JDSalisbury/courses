13453a9acd11cdbf21e741fd0456f3fd1657759b
JDSalisbury <salisbury.jeffery@gmail.com>
2018-02-15 14:24:30 -0500

    made CourseRepo i think

--- src/main/java/org/wecancodeit/courses/CourseRepo.java
+++ src/main/java/org/wecancodeit/courses/CourseRepo.java
@@ -1,7 +1,16 @@
 package org.wecancodeit.courses;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CourseRepo {
 	
+	Map<String, Course> courses = new HashMap<>();
 	
+	
+	public Collection<Course> courseList(){
+		return courses.values();
+	}
 	
 }
